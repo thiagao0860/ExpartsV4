bd09e1a6d15b2d0987c87fe90a0b6b426882b966
thiagao0860 <41775641+thiagao0860@users.noreply.github.com>
2021-03-24 01:45:43 -0300

    Title Bar Configuration

--- App.h
+++ App.h
@@ -10,5 +10,7 @@ namespace winrt::ExpartsV4::implementation
         void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
         void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
         void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+        void App::OnVisibleChanged(Windows::ApplicationModel::Core::CoreApplicationViewTitleBar sender, IInspectable const&);
+        void App::OnLayoutMetricsChanged(Windows::ApplicationModel::Core::CoreApplicationViewTitleBar sender, IInspectable const&);
     };
 }
