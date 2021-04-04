ae2446f2978fff5780179e6eb1300a8e3640c3f8
thiagao0860 <41775641+thiagao0860@users.noreply.github.com>
2021-04-04 18:38:48 -0300

    Console working

--- ContentPage/MonitorRTPage.cpp
+++ ContentPage/MonitorRTPage.cpp
@@ -21,27 +21,27 @@ namespace winrt::ExpartsV4::implementation
     
     void MonitorRTPage::SearchButton_onClick(IInspectable const& sender, RoutedEventArgs const& args)
     {
-        SourceModeDialog dialog { make<SourceModeDialog>()};
-        dialog.Closed({this , &MonitorRTPage::DerivedContentDialog_onClose });
+        SourceModeDialog dialog;
+        dialog.Closed({ this , &MonitorRTPage::DerivedContentDialog_onClose });
         dialog.ShowAsync();
     }
 
     void MonitorRTPage::RefreshButton_onClick(IInspectable const& sender, RoutedEventArgs const& args)
     {
-        OutputDebugString(L"Refresh Button Click\n");
+        putConsoleMessage(L"Refresh Button Click");
     }
 
     void MonitorRTPage::StartLearningButton_onClick(IInspectable const& sender, RoutedEventArgs const& args)
     {
-        OutputDebugString(L"Start Learning Button Click\n");
+        putConsoleMessage(L"Start Learning Button Click");
     }
     void MonitorRTPage::sliderMinSensitiveCutOff_onVAlueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::Primitives::IRangeBaseValueChangedEventArgs const& args)
     {
-        OutputDebugString((to_hstring(args.NewValue()) + to_hstring(" to ") + to_hstring(args.NewValue()) + to_hstring(" \n")).c_str());
+       // putConsoleMessage((to_hstring(args.NewValue()) + to_hstring(" to ") + to_hstring(args.NewValue()) + to_hstring(" \n")).c_str());
     }
     void MonitorRTPage::sliderMaxSensitiveCutOff_onVAlueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::Primitives::IRangeBaseValueChangedEventArgs const& args)
     {
-        OutputDebugString((to_hstring(args.NewValue()) +to_hstring(" to ")+ to_hstring(args.NewValue()) +to_hstring(" \n")).c_str());
+      //  putConsoleMessage((to_hstring(args.NewValue()) +to_hstring(" to ")+ to_hstring(args.NewValue()) +to_hstring(" \n")).c_str());
     }
 
     void MonitorRTPage::DerivedContentDialog_onClose(winrt::Windows::UI::Xaml::Controls::ContentDialog const& sender, winrt::Windows::UI::Xaml::Controls::ContentDialogClosedEventArgs const& args)
@@ -49,7 +49,7 @@ namespace winrt::ExpartsV4::implementation
         SourceModeDialog dialog = (sender.try_as<SourceModeDialog>());
         switch (dialog.SelectedMode())
         {
-        case ConnectionModeOPTs::LOCAL_CONNECTION:
+        case (INT16)ConnectionModeOPTs::LOCAL_CONNECTION:
 
             break;
         default:
@@ -59,7 +59,9 @@ namespace winrt::ExpartsV4::implementation
 
     void MonitorRTPage::putConsoleMessage(winrt::hstring message)
     {
-        TextBlock text{ make<TextBlock>(message)};
+        TextBlock text;
+        text.Text(message);
+        text.Margin(ThicknessHelper::FromUniformLength(2));
         this->StackPanelConsole().Children().Append(text);
     }
 
